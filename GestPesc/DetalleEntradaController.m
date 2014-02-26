e201db2b9f99bf26101fddbeee05f52d74004eed
Luis Ángel García Muñoz <lagm@tid.es>
2014-02-26 17:53:15 +0100

    - Creación de entradas completado

--- GestPesc/DetalleEntradaController.m
+++ GestPesc/DetalleEntradaController.m
@@ -26,7 +26,8 @@
 @property (strong, nonatomic) PFObject *entrada;
 @property NSDateFormatter *formatoFecha;
 @property BOOL nuevaEntrada;
-@property (assign, nonatomic) id currentResponder;
+@property (assign, nonatomic) UITextField *currentResponder;
+@property (assign, nonatomic) UITextView *currentResponderTV;
 
 - (IBAction)cogerFotoEntrada:(id)sender;
 - (IBAction)guardarEntrada:(id)sender;
@@ -36,6 +37,7 @@
 
 @implementation DetalleEntradaController {
     NSArray *resultadosBusqueda;
+    NSString *kTituloArticulo;
 }
 
 - (void)viewDidLoad {
@@ -43,11 +45,44 @@
     self.formatoFecha = [[NSDateFormatter alloc] init];
     [self.formatoFecha setDateFormat:@"dd/MM/yyyy"];
     [self configureView];
+    // Se pone el título por defecto al nombre del Artículo
+    kTituloArticulo = @"Introduce aquí el artículo...";
+    _nombreProductoLabel.text = kTituloArticulo;
     // Creamos un nuevo notificador para refrescar los datos de la tabla
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(refrescaEntrada:)
                                                  name:@"refrescaEntrada"
                                                object:nil];
+    // Esto sirve para añadir botones de "Ok" y "Cancelar" a los teclados numéricos, que no tienen
+    UIToolbar* numberToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 32)];
+    numberToolbar.items = [NSArray arrayWithObjects:
+                           [[UIBarButtonItem alloc]initWithTitle:@"Cancelar" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelarNumberPad)],
+                           [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
+                           [[UIBarButtonItem alloc]initWithTitle:@"Ok" style:UIBarButtonItemStyleDone target:self action:@selector(okNumberPad)],
+                           nil];
+    _stockTotalTextField.inputAccessoryView = numberToolbar;
+    _precioTextField.inputAccessoryView = numberToolbar;
+    _stockDisponibleTextField.inputAccessoryView = numberToolbar;
+    _formatoCajaTextField.inputAccessoryView = numberToolbar;
+    _comentariosTextView.inputAccessoryView = numberToolbar;
+}
+
+- (void)cancelarNumberPad {
+    [self.currentResponder resignFirstResponder];
+    [self.currentResponderTV resignFirstResponder];
+}
+
+- (void)okNumberPad {
+    if (self.currentResponder == _stockTotalTextField) {
+        [_precioTextField becomeFirstResponder];
+    } else if (self.currentResponder == _precioTextField) {
+        [_stockDisponibleTextField becomeFirstResponder];
+    } else if (self.currentResponder == _stockDisponibleTextField) {
+        [_formatoCajaTextField becomeFirstResponder];
+    } else {
+        [self.currentResponder resignFirstResponder];
+    }
+    [self.currentResponderTV resignFirstResponder];
 }
 
 - (void)refrescaEntrada:(NSNotification *)notification {
@@ -104,37 +139,64 @@
     }
     return [super tableView:tableView heightForRowAtIndexPath:indexPath];
 }
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.currentResponder resignFirstResponder];
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+}
 */
 
 - (BOOL)tableView:(UITableView *)tableView shouldHighlightRowAtIndexPath:(NSIndexPath *)indexPath {
     return YES;
 }
 
-/*
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [self.currentResponder resignFirstResponder];
-    [tableView deselectRowAtIndexPath:indexPath animated:NO];
-}
-*/
-
 - (IBAction)guardarEntrada:(id)sender {
     NSLog(@"guardarEntrada");
-    // Actualizamos los datos del objeto entrada
-    // TODO: Comprobar datos de entrada
+    // Se crea una entrada nueva
     @try {
-        /*
-        self.entrada[@"nombre"] = _nombreProductoTextField.text;
+        // Datos obligatorios
+        if (!_entrada[@"fk_articulo"]) {
+            [LAUtils alertStatus:@"Tienes que elegir un Artículo" withTitle:@"Nueva entrada" andDelegate:self];
+            return;
+        }
+        if ([_stockTotalTextField.text isEqualToString:@""]) {
+            [LAUtils alertStatus:@"Tienes que introducir el stock total" withTitle:@"Nueva entrada" andDelegate:self];
+            return;
+        }
         self.entrada[@"stock_total"] = @([_stockTotalTextField.text integerValue]);
+        if ([_precioTextField.text isEqualToString:@""]) {
+            [LAUtils alertStatus:@"Tienes que introducir el precio unitario" withTitle:@"Nueva entrada" andDelegate:self];
+            return;
+        }
         self.entrada[@"precio"] = @([_precioTextField.text integerValue]);
+        if ([_stockDisponibleTextField.text isEqualToString:@""]) {
+            [LAUtils alertStatus:@"Tienes que introducir el stock disponible" withTitle:@"Nueva entrada" andDelegate:self];
+            return;
+        }
         self.entrada[@"stock_disponible"] = @([_stockDisponibleTextField.text integerValue]);
+        if ([_formatoCajaTextField.text isEqualToString:@""]) {
+            [LAUtils alertStatus:@"Tienes que introducir el formato de caja" withTitle:@"Nueva entrada" andDelegate:self];
+            return;
+        }
         self.entrada[@"formato_caja"] = @([_formatoCajaTextField.text integerValue]);
-        self.entrada[@"descripcion"] = _fechaEnvioTextField.text;
-        //    [self.entrada setObject:_fechaEnvioTextField.text forKey:@"entregado"];
+        if (!_fechaEnvioDatePicker.date) {
+            [LAUtils alertStatus:@"Tienes que introducir fecha de envío" withTitle:@"Nueva entrada" andDelegate:self];
+            return;
+        }
+        self.entrada[@"entregado"] = _fechaEnvioDatePicker.date;
+        
+        // Datos no obligatorios
+        self.entrada[@"observacion"] = _comentariosTextView.text;
         // Imagen en JPG, con escasa compresión
-        NSData *imageData = UIImageJPEGRepresentation(_fotoProducto.image, 0.8);
-        PFFile *imageFile = [PFFile fileWithName:@"foto.jpg" data:imageData];
-        self.entrada[@"imagen"] = imageFile;
-         */
+        if (_fotoProductoImage.image) {
+            NSData *imageData = UIImageJPEGRepresentation(_fotoProductoImage.image, 0.8);
+            PFFile *imageFile = [PFFile fileWithName:@"foto.jpg" data:imageData];
+            self.entrada[@"imagen"] = imageFile;
+        }
+        // El proveedor es el usuario actual logueado
+        self.entrada[@"fk_proveedor"] = [PFUser currentUser];
+        // Flag de comprobado a False
+        self.entrada[@"comprobado"] = @NO;
     } @catch (NSException *exception) {
         [LAUtils alertStatus:@"Datos no válidos, revíselos" withTitle:@"Error" andDelegate:self];
     }
@@ -210,7 +272,7 @@
 }
 
 - (void)textViewDidBeginEditing:(UITextView *)textView {
-    self.currentResponder = textView;
+    self.currentResponderTV = textView;
 }
 
 - (BOOL)textViewShouldEndEditing:(UITextView *)textView {
@@ -218,10 +280,6 @@
     return YES;
 }
 
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    [textField resignFirstResponder];
-    return YES;
-}
 
 @end
 
